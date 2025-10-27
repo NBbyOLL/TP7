8d322760e8e2db63083c01bebd60de8480f189d8
NBbyOLL <ayoub.ouahaa@gmail.com>
2025-10-28 00:49:19 +0100

    Réalisation TP7

---
+++ src/main/java/ma/ws/jaxrs/Entity/TypeCompte.java
@@ -0,0 +1,5 @@
+package ma.ws.jaxrs.Entity;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ src/main/java/ma/ws/jaxrs/repository/CompteRepository.java
@@ -0,0 +1,7 @@
+package ma.ws.jaxrs.repository;
+
+import ma.ws.jaxrs.Entity.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
